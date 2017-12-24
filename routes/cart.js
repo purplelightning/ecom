67e6c305392cc1fe76e214bc09449e7e4be8746f
purplelightning <1979506141@qq.com>
2017-12-24 20:03:02 +0800

    购物车页面的item模板实现

--- routes/cart.js
+++ routes/cart.js
@@ -1,5 +1,8 @@
-module.exports=function (app) {
-  app.get('/cart',function (req, res) {
+module.exports = function (app) {
+  app.get('/cart', function (req, res) {
     res.render('cart')
+  })
+  app.post('/cart', function (req, res) {
+
   })
 }
